c3bf58d929220574511772923aba1098c97e2ccb
Palnit <105753906+Palnit@users.noreply.github.com>
2025-08-08 21:21:54 +0200

    Test github workflow 4

--- modules/Math/Tests/src/vector_test.cpp
+++ modules/Math/Tests/src/vector_test.cpp
@@ -5,6 +5,7 @@ template<typename T>
 class VectorTest : public testing::Test {
 protected:
     using VectorType = T;
+    using ValueType = typename VectorType::ValueType;
     VectorTest() : rhs(5) {
         for (size_t i = 0; i < T::Size(); ++i) {
             lhs[i] = i + 1;
@@ -33,16 +34,23 @@ using VectorTypes = ::testing::Types<HUH::Vector2f,
 TYPED_TEST_SUITE(VectorTest, VectorTypes);
 
 TYPED_TEST(VectorTest, Initializations) {
-    for (size_t i = 0; i < TestFixture::VectorType::Size(); ++i) {
-        EXPECT_EQ(this->lhs[i], i + 1);
-        EXPECT_EQ(this->rhs[i], 5);
+    for (int i = 0; i < TestFixture::VectorType::Size(); ++i) {
+        EXPECT_EQ(this->lhs[i], static_cast<typename TestFixture::ValueType>(i + 1));
+        EXPECT_EQ(this->rhs[i], static_cast<typename TestFixture::ValueType>(5));
     }
 }
 
 TYPED_TEST(VectorTest, Addition) {
     const auto result = this->lhs + this->rhs;
     EXPECT_EQ(result.Size(), TestFixture::VectorType::Size());
-    for (size_t i = 0; i < TestFixture::VectorType::Size(); ++i) {
-        EXPECT_EQ(result[i], (i + 1) + 5);
+    for (int i = 0; i < TestFixture::VectorType::Size(); ++i) {
+        EXPECT_EQ(result[i], static_cast<typename TestFixture::ValueType>((i + 1) + 5));
+    }
+}
+TYPED_TEST(VectorTest, Subtraction) {
+    const auto result = this->lhs - this->rhs;
+    EXPECT_EQ(result.Size(), TestFixture::VectorType::Size());
+    for (int i = 0; i < TestFixture::VectorType::Size(); ++i) {
+        EXPECT_EQ(result[i], static_cast<typename TestFixture::ValueType>((i + 1) - 5));
     }
 }
