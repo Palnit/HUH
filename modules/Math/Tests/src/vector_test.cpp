3011269e56cb9bf9e8d320edaad3744fb9b341d2
Palnit <105753906+Palnit@users.noreply.github.com>
2025-08-08 21:24:48 +0200

    Test github workflow 5

--- modules/Math/Tests/src/vector_test.cpp
+++ modules/Math/Tests/src/vector_test.cpp
@@ -54,3 +54,11 @@ TYPED_TEST(VectorTest, Subtraction) {
         EXPECT_EQ(result[i], static_cast<typename TestFixture::ValueType>((i + 1) - 5));
     }
 }
+
+TYPED_TEST(VectorTest, Multiplication) {
+    const auto result = this->lhs * this->rhs;
+    EXPECT_EQ(result.Size(), TestFixture::VectorType::Size());
+    for (int i = 0; i < TestFixture::VectorType::Size(); ++i) {
+        EXPECT_EQ(result[i], static_cast<typename TestFixture::ValueType>((i + 1) * 5));
+    }
+}
