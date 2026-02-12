f9a8f4170b5da0a0b974ce0533f4939171d1b793
Palnit <105753906+Palnit@users.noreply.github.com>
2026-02-12 15:44:30 +0100

    Windows console redirecting tmp

--- modules/Core/platform/include/HUH/Windows/win_main.h
+++ modules/Core/platform/include/HUH/Windows/win_main.h
@@ -9,12 +9,96 @@ inline void WinError() {
     MessageBox(nullptr, "HUH Win init error", "Error reading windows cmd args", 0);
 }
 
+inline void WinConsoleRelease() {
+    FILE* fp;
+
+    // Just to be safe, redirect standard IO to NUL before releasing.
+
+    // Redirect STDIN to NUL
+    if (freopen_s(&fp, "NUL:", "r", stdin) != 0) {
+        HUH::WinError();
+    } else {
+        setvbuf(stdin, nullptr, _IONBF, 0);
+    }
+
+    // Redirect STDOUT to NUL
+    if (freopen_s(&fp, "NUL:", "w", stdout) != 0) {
+        HUH::WinError();
+    } else {
+        setvbuf(stdout, nullptr, _IONBF, 0);
+    }
+
+    // Redirect STDERR to NUL
+    if (freopen_s(&fp, "NUL:", "w", stderr) != 0) {
+        HUH::WinError();
+    } else {
+        setvbuf(stderr, nullptr, _IONBF, 0);
+    }
+
+    // Detach from console
+    if (!FreeConsole()) {
+        HUH::WinError();
+    }
+}
+
+inline void WinConsoleRedirect() {
+    WinConsoleRelease();
+    if (!AttachConsole(ATTACH_PARENT_PROCESS)) {
+        if (!AllocConsole()) {
+            HUH::WinError();
+        }
+    }
+
+    FILE* fp;
+
+    // Redirect STDIN if the console has an input handle
+    if (GetStdHandle(STD_INPUT_HANDLE) != INVALID_HANDLE_VALUE) {
+        if (freopen_s(&fp, "CONIN$", "r", stdin) != 0) {
+            HUH::WinError();
+        } else {
+            setvbuf(stdin, nullptr, _IONBF, 0);
+        }
+    }
+
+    // Redirect STDOUT if the console has an output handle
+    if (GetStdHandle(STD_OUTPUT_HANDLE) != INVALID_HANDLE_VALUE) {
+        if (freopen_s(&fp, "CONOUT$", "w", stdout) != 0) {
+            HUH::WinError();
+        } else {
+            setvbuf(stdout, NULL, _IONBF, 0);
+        }
+    }
+
+    // Redirect STDERR if the console has an error handle
+    if (GetStdHandle(STD_ERROR_HANDLE) != INVALID_HANDLE_VALUE) {
+
+        if (freopen_s(&fp, "CONOUT$", "w", stderr) != 0) {
+            HUH::WinError();
+        } else {
+            setvbuf(stderr, NULL, _IONBF, 0);
+        }
+    }
+
+    // Make C++ standard streams point to console as well.
+    std::ios::sync_with_stdio(true);
+
+    // Clear the error state for each of the C++ standard streams.
+    std::wcout.clear();
+    std::cout.clear();
+    std::wcerr.clear();
+    std::cerr.clear();
+    std::wcin.clear();
+    std::cin.clear();
+}
+
 }// namespace HUH
 
 extern "C" {
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR szCmdLine, int nCmdShow) {
     HUH::g_AppInstance = hInst;
     HUH::g_CmdShow = nCmdShow;
+
+    HUH::WinConsoleRedirect();
 
     LPWSTR cmdLineFull = GetCommandLineW();
     const int argSize = WideCharToMultiByte(CP_UTF8, 0, cmdLineFull, -1, nullptr, 0, nullptr, nullptr);
@@ -59,6 +143,8 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR szCmdLine, int nCmdSh
     const auto result = HUH_MAIN(argc, argv);
     LocalFree(argvW);
     std::free(argv);
+
+    HUH::WinConsoleRelease();
     return result;
 }
 }
